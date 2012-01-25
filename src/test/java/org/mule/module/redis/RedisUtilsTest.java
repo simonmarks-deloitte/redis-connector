e40fc4aef4ce6da84a5899dc95fa16df2e21c924
Franco Bulgarelli <franco@zaubersoftware.com>
2012-01-25 12:40:22 -0300

    More tests

--- src/test/java/org/mule/module/redis/RedisUtilsTest.java
+++ src/test/java/org/mule/module/redis/RedisUtilsTest.java
@@ -8,11 +8,13 @@
  * LICENSE.txt file.
  */
 
-
 package org.mule.module.redis;
 
-import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 import java.io.Serializable;
 import java.util.Arrays;
@@ -23,6 +25,11 @@ import org.junit.experimental.theories.DataPoints;
 import org.junit.experimental.theories.Theories;
 import org.junit.experimental.theories.Theory;
 import org.junit.runner.RunWith;
+import org.mule.module.redis.RedisUtils.RedisAction;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.exceptions.JedisConnectionException;
 
 /**
  * @author flbulgarelli
@@ -32,7 +39,7 @@ public class RedisUtilsTest
 {
     @DataPoints
     public static final Serializable[] SERIALIZABLES = {"Hello World", 340,
-        (Serializable) Arrays.asList(4, 5, 6), TimeUnit.HOURS, null, /*XXX "" */ };
+        (Serializable) Arrays.asList(4, 5, 6), TimeUnit.HOURS, null, /* XXX "" */};
 
     @Theory
     public void fromBytesIsInverseOfFromBytes(Serializable value)
@@ -40,6 +47,45 @@ public class RedisUtilsTest
         assertEquals(value, RedisUtils.fromBytes(RedisUtils.toBytes(value)));
     }
 
+    @Test
+    public void runAnswersActionResultWhenSucceds() throws Exception
+    {
+        JedisPool poolMock = mock(JedisPool.class);
+        String result = RedisUtils.run(poolMock, new RedisAction<String>()
+        {
+            @Override
+            public String run()
+            {
+                return "Hello";
+            }
+        });
+        assertEquals("Hello", result);
+        verify(poolMock).getResource();
+        verify(poolMock).returnResource(any(Jedis.class));
+    }
 
+    @Test
+    public void runThrowsExceptionWhenActionFails() throws Exception
+    {
+        JedisPool poolMock = mock(JedisPool.class);
+        try
+        {
+            RedisUtils.run(poolMock, new RedisAction<String>()
+            {
+                @Override
+                public String run()
+                {
+                    throw new JedisConnectionException("ups!");
+                }
+            });
+            fail();
+        }
+        catch (Exception e)
+        {
+            //OK
+        }
+        verify(poolMock).getResource();
+        verify(poolMock).returnBrokenResource(any(Jedis.class));
+    }
 
 }
